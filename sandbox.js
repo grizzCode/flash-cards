40650b4fa159f2b3bf8cd366df523fc18bfa3351
Charles Buxton <chazbux@gmail.com>
2020-03-05 16:42:36 -0700

    stable card flip

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,8 @@
-let cardIndex = [1,2,3]
+let cards = [
+  {id:1, front: "Front String A", back: "Back String A"},
+  {id:2, front: "Front String B", back: "Back String B"},
+  {id:3, front: "Front String C", back: "Back String C"},
+]
+let cardBack = cards[2].back
 
-let newId = cardIndex.length + 1
+console.log(cardBack)
--- scripts.js
+++ scripts.js
@@ -1,21 +1,44 @@
 let cardIndex = [1,2,3]
-// Get last item of array
-let id = cardIndex.length + 1;
+// Accumulator for card ID's
+let rowNums = 1
+//Accumulator for row numbers (formatting)
+let cards = [
+  {id:1, front: "Front String A", back: "Back String A"},
+  {id:2, front: "Front String B", back: "Back String B"},
+  {id:3, front: "Front String C", back: "Back String C"},
+]
 
-
-
-document.getElementById('1').onclick = () => {
-    document.getElementById('1').innerHTML = "New Text";
+function flipCard(id){
+    let back = cards[(id-1)].back
+    document.getElementById(id).innerHTML = `${back}`;
 }
 
+
 document.getElementById('newCardButton').onclick = () => {
-  console.log("test")
-  // let div = document.createElement('div');
-  
-  
-  // div.textContent = "Sup, y'all?";
-  // div.setAttribute('class', 'four columns');
-  // div.setAttribute('id', id);
-  // document.body.appendChild(div);
+  let id = cardIndex.length + 1;
+  cardIndex.push(id)
+
+  if (id % 3 == 0) {
+    let row = document.createElement('row');
+    document.body.appendChild(row);
+    let div = document.createElement('div');
+    div.setAttribute('class', 'four columns');
+    div.setAttribute('id', id);
+
+    let front = document.createElement('form')
+    front.setAttribute('placeholder', "New Card Front")
+    
+    row.appendChild(div);
+  }
+
+  let div = document.createElement('div');
+    div.setAttribute('class', 'four columns');
+    div.setAttribute('id', id);
+
+  let front = document.createElement('text-input')
+    front.setAttribute('placeholder', "New Card Front")
+    
+  document.body.appendChild(div);
+  div.appendChild(front);
 }
 
