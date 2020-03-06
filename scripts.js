a3099479e104ed82737c565e80d84c201fbbe506
Charles Buxton <chazbux@gmail.com>
2020-03-05 23:05:58 -0700

    edit button work

--- scripts.js
+++ scripts.js
@@ -18,7 +18,89 @@ function flipCard(id){
 function deleteCard(id) {
   document.getElementById(id).remove()
 }
+
+//################ EDIT CARD ####################
+function editCard(id ) {
   
+
+  //creates new div for formatting
+  let row = document.createElement('row');
+  document.body.appendChild(row);
+  let div = document.createElement('div');
+  div.setAttribute('class', 'four columns');
+  div.setAttribute('id', `${newId}`);
+  
+
+  //create user input area for front of card:
+  let form = document.createElement('form')
+    form.setAttribute('id', 'newForm')
+    div.appendChild(form)
+  let input1 = document.createElement('input')
+    input1.setAttribute('type', 'text');
+    input1.setAttribute('id', 'input1');
+    input1.setAttribute('placeholder', 'Enter Card Front');
+    input1.setAttribute('size', '45');
+    form.appendChild(input1)
+    //create a hr tag for formatting:
+      let hr = document.createElement('hr')
+      form.appendChild(hr)
+  //create second user input area for back of card:
+      let input2 = document.createElement('input')
+    input2.setAttribute('type', 'text');
+    input2.setAttribute('id', 'input2');
+    input2.setAttribute('placeholder', 'Enter Card Back');
+    input2.setAttribute('size', '45');
+    form.appendChild(input2)
+  //create a button to save:
+  let saveButton = document.createElement('button')
+    saveButton.setAttribute('type', 'button');
+    saveButton.setAttribute('id', 'saveButton');
+    saveButton.innerHTML = "Save Changes";
+    form.appendChild(saveButton)
+
+  row.appendChild(div);
+ 
+  // ###################### SET CARD EDITS ######################
+  //Obatin input values once user enters them:
+  document.getElementById('saveButton').onclick = () => {
+    let inputVal1 = document.getElementById('input1').value;
+    let inputVal2 = document.getElementById('input2').value;
+    cards[id-1].front = inputVal1
+    cards[id-1].back = inputVal2
+    
+    // clear form from new card:
+    document.getElementById('newForm').remove();
+
+    // display new card front:
+    let newFront = document.createElement('p')
+    newFront.innerHTML = cards[(newId-1)].front
+    // generate flip card button:
+    let newFlipButton = document.createElement('button')
+    newFlipButton.setAttribute('type', 'button')
+    newFlipButton.setAttribute('onclick', `flipCard(${newId})`)
+    newFlipButton.innerHTML = "Flip Card"
+    // generate edit card button:
+    let newEditButton = document.createElement('button')
+    newEditButton.setAttribute('type', 'button')
+    // newDeleteButton.setAttribute('onclick', `flipCard(${newId})`)
+    newEditButton.innerHTML = "Edit Card"
+    // Generate delete button
+    let newDeleteButton = document.createElement('button')
+    newDeleteButton.setAttribute('type', 'button')
+    newDeleteButton.setAttribute('onclick', `deleteCard(${newId})`)
+    newDeleteButton.innerHTML = "Delete Card"
+
+
+    div.appendChild(newFront)
+    div.appendChild(newFlipButton)
+    div.appendChild(newEditButton)
+    div.appendChild(newDeleteButton)
+  }
+
+}
+
+
+
 
 //############### CREATE NEW CARD #################
 document.getElementById('newCardButton').onclick = () => {
